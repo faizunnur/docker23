5147d7bab42456392ec5e7d6d38ca217cb7ca5cb
faizunnur <faizunnur71@gmail.com>
2026-03-25 21:47:45 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/docker23/Docker23Application.java
@@ -0,0 +1,13 @@
+package com.example.docker23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docker23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Docker23Application.class, args);
+	}
+
+}
